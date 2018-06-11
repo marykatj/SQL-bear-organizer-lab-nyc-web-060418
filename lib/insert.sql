9adbc1e9dd74d575f9559230884534666322c933
Mary-Kate James <marykatj@gmail.com>
2018-06-11 01:28:50 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (1, 'Mr. Chocolate', 27, 'M', 'brown', 'calm', 0);
-INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (1, 'Mr. Chocolate', 27, 'M', 'brown', 'calm', 0);
-INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (1, 'Mr. Chocolate', 27, 'M', 'brown', 'calm', 0);
-INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (1, 'Mr. Chocolate', 27, 'M', 'brown', 'calm', 0);
-INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (1, 'Mr. Chocolate', 27, 'M', 'brown', 'calm', 0);
-INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (1, 'Mr. Chocolate', 27, 'M', 'brown', 'calm', 0);
-INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (1, 'Mr. Chocolate', 27, 'M', 'brown', 'calm', 0);
-INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (1, 'Mr. Chocolate', 27, 'M', 'brown', 'calm', 0);
+INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (2, 'Mr. Chocolate', 30, 'F', 'brown', 'calm', 1);
+INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (3, 'Mr. Chocolate', 7, 'M', 'purple', 'calm', 0);
+INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (4, 'Mr. Chocolate', 10, 'F', 'brown', 'calm', 1);
+INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (5, 'Mr. Chocolate', 1, 'M', 'brown', 'calm', 0);
+INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (6, 'Mr. Chocolate', 70, 'F', 'brown', 'calm', 1);
+INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (7, 'Mr. Chocolate', 99, 'M', 'brown', 'calm', 0);
+INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (8, 'Mr. Chocolate', 2, 'M', 'brown', 'calm', 0);
