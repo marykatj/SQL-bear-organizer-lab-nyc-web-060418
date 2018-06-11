5a1844fd590d0acd10b6cc51f7dbd17bb5e01f0a
Mary-Kate James <marykatj@gmail.com>
2018-06-11 01:27:50 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (1, 'Mr. Chocolate', 27, 'M', 'brown', 'calm', 0);
-INSERT INTO bears (name, age, color) VALUES ('Rowdy', 21, 'brown');
-INSERT INTO bears (name, age, color) VALUES ('Tabitha', 2, 'brown');
-INSERT INTO bears (name, age, color) VALUES ('Sergeant Brown', 3, 'brown');
-INSERT INTO bears (name, age, color) VALUES ('Melissa', 100, 'brown');
-INSERT INTO bears (name, age, color) VALUES ('Grinch', 16, 'brown');
-INSERT INTO bears (name, age, color) VALUES ('Wendy', 9, 'brown');
-INSERT INTO bears (name, age, color) VALUES (NULL, 8, 'black');
+INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (1, 'Mr. Chocolate', 27, 'M', 'brown', 'calm', 0);
+INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (1, 'Mr. Chocolate', 27, 'M', 'brown', 'calm', 0);
+INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (1, 'Mr. Chocolate', 27, 'M', 'brown', 'calm', 0);
+INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (1, 'Mr. Chocolate', 27, 'M', 'brown', 'calm', 0);
+INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (1, 'Mr. Chocolate', 27, 'M', 'brown', 'calm', 0);
+INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (1, 'Mr. Chocolate', 27, 'M', 'brown', 'calm', 0);
+INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (1, 'Mr. Chocolate', 27, 'M', 'brown', 'calm', 0);
