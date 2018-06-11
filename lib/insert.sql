b8e78666f460799dcf679f16008dad80d7608b92
Mary-Kate James <marykatj@gmail.com>
2018-06-11 01:27:15 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES ('Mr. Chocolate', 27, 'brown');
+INSERT INTO bears (id, name, age, gender, color, temperment, alive) VALUES (1, 'Mr. Chocolate', 27, 'M', 'brown', 'calm', 0);
 INSERT INTO bears (name, age, color) VALUES ('Rowdy', 21, 'brown');
 INSERT INTO bears (name, age, color) VALUES ('Tabitha', 2, 'brown');
 INSERT INTO bears (name, age, color) VALUES ('Sergeant Brown', 3, 'brown');
